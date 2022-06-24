baf2dafe349f4b05c741db14cb2dc725daf87eff
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-24 18:30:03 +0000

    Fri, Jun 24, 2022, 6:30 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
+
 int main(void)
 {
+// ask for a name
     string name = get_string("What is your name? \n");
-
+//print the hello "name"
     printf("hello, %s \n", name);
 }
