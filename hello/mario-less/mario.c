e3c014a16f9b974e8b74b1fba0a9211970b637c5
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-24 20:36:45 +0000

    Fri, Jun 24, 2022, 8:36 PM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -10,9 +10,8 @@ int main(void)
     }
     while (n < 1 || n > 8);
 
-    for(int i = 0; i < n-(n-1) + 1; i++)
+    for(int i = 0; i < n; i++)
     {
-        printf("#");
         for(int j = 0; j < n; j++)
         printf("#");
         printf("\n");
