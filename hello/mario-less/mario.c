0295fdfd74ccc61fb885442474027d347c871aa9
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-24 18:54:38 +0000

    Fri, Jun 24, 2022, 6:54 PM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -3,5 +3,15 @@
 
 int main(void)
 {
+    // ask for the height of the pyramid
+do
+    integer height = get_int("Height: ")
 
+while (height < 1)
 }
+
+// if not given the correct answer, keep asking for the height.
+
+
+//if the height is 2 start with the 1 hashtag at the top,
+//loop down the pyramid until the base matches the height number
