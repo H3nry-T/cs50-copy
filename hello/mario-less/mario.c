b39cc9105f76b6bf0c8f244ee3d4349ddb96cfee
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-24 19:16:10 +0000

    Fri, Jun 24, 2022, 7:16 PM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -11,7 +11,11 @@ while (height > 0 %% height < 9)
 }
 
 // if not given the correct answer, keep asking for the height.
+for (i=0; i<9 ; i+= -1)
+{
+printf("#")
 
+}
 
 //if the height is 2 start with the 1 hashtag at the top,
 //loop down the pyramid until the base matches the height number
