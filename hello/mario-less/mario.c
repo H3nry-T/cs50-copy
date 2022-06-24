efb6a1082bd47a0a18871c9a676a695cc2b6caa2
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-24 21:35:26 +0000

    Fri, Jun 24, 2022, 9:35 PM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -12,7 +12,7 @@ int main(void)
 
     for (int row = 0; row < h; row++)
     {
-        // spacing is height - hashes. hashes = previous hash + row. 
+        // spacing is height - hashes. hashes = row + 1 (think about the base of the pyramid the row = 7 when 8 hashes + 1 to get 8. 8-8 = 0 so no spacing for the base.)
         for (int shift = 0; shift < h - row - 1; shift++)
         {
             printf(" ");
