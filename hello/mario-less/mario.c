6f4433f2205b1fafa502cf52cccca118e291962c
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-24 19:52:08 +0000

    Fri, Jun 24, 2022, 7:52 PM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -1,11 +1,11 @@
-#include <cs50.h>
+#include <CS50.h>
 #include <stdio.h>
 
 int main(void)
 {
     // ask for the height of the pyramid
 do
-    integer height = get_int("Height: ")
+    height = get_int("Height: ")
         printf("height is now: \n")
 while (height > 0 %% height < 9)
 }
@@ -13,7 +13,7 @@ while (height > 0 %% height < 9)
 // if not given the correct answer, keep asking for the height.
 for (i=0; i<9 ; i+= -1)
 {
-printf("#")
+printf("# \n")
 
 }
 
