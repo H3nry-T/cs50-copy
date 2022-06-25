96c4038352f0dca998d726e254236ee86aff8893
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-25 20:20:27 +0000

    Sat, Jun 25, 2022, 8:20 PM +00:00

--- pset1/mario-more/mario.c
+++ pset1/mario-more/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h < 0 || h > 8);
+
+    for (i = 0; i + )
 }
