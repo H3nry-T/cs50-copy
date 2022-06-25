4aee2a78ffe3c4b25cb2bfd57725726b6ebfdd3f
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-25 11:51:28 +0000

    Sat, Jun 25, 2022, 11:51 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -51,7 +51,7 @@ int calculate_quarters(int cents)
 {
     // TODO
     int quarters = 0;
-    while (cents <= 25)
+    while (cents >= 25)
     {
         quarters++;
         cents = cents - 25;
@@ -62,17 +62,35 @@ int calculate_quarters(int cents)
 int calculate_dimes(int cents)
 {
     // TODO
-    return 0;
+    int dimes = 0;
+    while (cents >= 10)
+    {
+        dimes++;
+        cents = cents - 10;
+    }
+    return dimes;
 }
 
 int calculate_nickels(int cents)
 {
     // TODO
-    return 0;
+    int nickels = 0;
+    while (cents >= 5)
+    {
+        nickels++;
+        cents = cents - 5;
+    }
+    return nickels;
 }
 
 int calculate_pennies(int cents)
 {
     // TODO
-    return 0;
+    int pennies = 0;
+    while (cents >= 1)
+    {
+        pennies++;
+        cents = cents - 1;
+    }
+    return pennies;
 }
