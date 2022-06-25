e59fbf25fa5a6343c1eeaccfa4e5100dba376c95
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-25 08:06:13 +0000

    Sat, Jun 25, 2022, 8:06 AM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -3,7 +3,11 @@
 
 int main(void)
 {
-    //get input of the height
-    int h; = get_int("Height: ");
-    printf("%i", h);
+    //get input of the height 1 - 8 inclusive
+    int h;
+    do
+    {
+    h = get_int("Height: ");
+    }
+    while(h < 1 || h > 8);
 }
