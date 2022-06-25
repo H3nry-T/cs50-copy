11d0dfa6d971b94535333f9d8afa443617f2256a
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-25 22:06:50 +0000

    Sat, Jun 25, 2022, 10:06 PM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
-int main(void) 
+bool valid_check(int number);
+
+int main(void)
 {
+
+}
+
+bool valid_check(int number);
+{
+    int = n;
     
+    n = get_int("Number: ");
+
 }
