1dca168b139e71c6f6316fcb552a00ee4c66d191
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-25 10:51:24 +0000

    Sat, Jun 25, 2022, 10:51 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -23,5 +23,14 @@ int main(void)
     // calculate the number of nickels to give the customer
     int nickels = calculate_nickels(cents);
     cents = cents - nickels * 5;
-    
+
+    //calculate the number of pennies given to the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    //sum the coins
+    int coins =  quarters + dimes + nickels + pennies ;
+
+    //print the number of coins
+    printf("%i ", coins);
 }
