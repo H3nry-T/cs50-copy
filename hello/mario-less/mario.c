b0108b084ec0d56f2b9cb4315497c86e72c706d5
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-25 08:33:53 +0000

    Sat, Jun 25, 2022, 8:33 AM +00:00

--- hello/mario-less/mario.c
+++ hello/mario-less/mario.c
@@ -10,7 +10,7 @@ int main(void)
     h = get_int("Height: ");
     }
     while(h < 1 || h > 8);
-// print the pyramid row and height. 
+// print the pyramid row and columns
     for(int i = 0; i < h; i++)
     {
         for(int j = 0; j < h; j++)
