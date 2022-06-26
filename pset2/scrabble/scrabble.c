cd26a903a40eaa7fe31105a1bd480b8f2b369fdb
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-26 21:43:33 +0000

    Sun, Jun 26, 2022, 9:43 PM +00:00

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -39,15 +39,20 @@ int compute_score(string word)
     int points = 0;
 
     for (int i = 0; i < strlen(word); i++)
-    {// if the word is upper case
+    {
+        // if the word is upper case
         if (isupper(word[i]))
-        {// the character of the word turns into an integer - 65 should equal position 0 IF the letter is A, rotates through the ascii codes until letter z
+        {
+            // the character of the word turns into an integer - 65 should equal position 0 IF the letter is A, rotates through the ascii codes until letter z
             points += POINTS[word[i] - 'A'];
-        }//word[i] is a character because stringarray is basically a character. A = 65 when placed in an arithmetic calculation.
+        }
+        //word[i] is a character because stringarray is basically a character. A = 65 when placed in an arithmetic calculation.
         else if (islower(word[i]))
-        {// in single quotation marks, a = 97 because of the ASCII coding
+        {
+            // in single quotation marks, a = 97 because of the ASCII coding
             points += POINTS[word[i] - 'a'];
-        }// if outside the alphabet ascii codes then do not give any points
+        }
+        // if outside the alphabet ascii codes then do not give any points
         else if (word[i] < 65 || word[i] > 122)
         {
             points += 0;
