5bf481e5a8d47d977e95145c7d3ddf31b0ed61e4
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-26 19:58:29 +0000

    Sun, Jun 26, 2022, 7:58 PM +00:00

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -19,9 +19,22 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    return 0;
 }
