e796862cb646372b921d02458f6c9d5c819d6325
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-26 22:10:50 +0000

    Sun, Jun 26, 2022, 10:10 PM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,10 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+int grade(string text);
 
 int main(void)
 {
+    string text = get_string("Text:");
+    int grade = grade(text);
+    printf("Grade %i\n",grade);
 
 }
 
+int grade(string text)
+{
+
+}
 // longer words
-//
+// longer sentences
+// index = o.0588 * L = 0.296 * S - 15.8
+//where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
