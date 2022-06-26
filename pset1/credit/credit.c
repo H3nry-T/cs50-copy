cbff644e89faae0f97dea50c9db1dbf464445f17
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-26 12:31:48 +0000

    Sun, Jun 26, 2022, 12:31 PM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -11,6 +11,10 @@ int main(void)
     do
     {
         n = get_long("Number: ");
+        if (n < 0 || (n != 13 && n != 15 && n != 16))
+        {
+            printf("INVALID\n");
+        }
     }
     while (n < 0 || (n != 13 && n != 15 && n != 16));
     // find alternating digits from the second last one, then times 2
