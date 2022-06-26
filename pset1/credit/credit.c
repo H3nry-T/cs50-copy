99669cb1612209683188d95c24a2c0fafc28b7dc
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-26 10:29:18 +0000

    Sun, Jun 26, 2022, 10:29 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -15,5 +15,7 @@ int main(void)
     while (n < 0);
     // in finding the second last digit, using the modulus of 100, this will get you the second last digit. 6872 / 100 = 68.72
     //take 72 and pick the first digit, of the remainder. which is the second last digit. (this REPEATS IN A PATTERN)
-    
+    // no1 is the SECOND LAST digit of the card
+    int no1, no2, no3, no4, no5, no6, no7, no8;
+    no1 = (n % 100) / 10
 }
