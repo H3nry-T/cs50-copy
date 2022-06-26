44d37bef24b1d153637604cd673500111fba9dec
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-26 10:36:12 +0000

    Sun, Jun 26, 2022, 10:36 AM +00:00

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -16,6 +16,12 @@ int main(void)
     // find alternating digits from the second last one, then times 2
     // no1 is the SECOND LAST digit of the card
     int no1, no2, no3, no4, no5, no6, no7, no8;
-    no1 = (n % 100) / 10
-    no2 = 
+    no1 = ((n % 100) / 10) * 2
+    no2 = ((n % 10000) / 1000) * 2
+    no3 = ((n % 1000000) / 100000) * 2
+    no4 = ((n % 1000000) / 100000) * 2
+    no5 = ((n % 1000000) / 100000) * 2
+    no6 = ((n % 1000000) / 100000) * 2
+    no7 = ((n % 1000000) / 100000) * 2
+    no8 = ((n % 1000000) / 100000) * 2
 }
