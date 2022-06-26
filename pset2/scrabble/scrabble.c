c18de31a53832868001c4bb7ce20579806e66dad
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-26 21:09:43 +0000

    Sun, Jun 26, 2022, 9:09 PM +00:00

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -36,6 +36,8 @@ int main(void)
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    int points = 0
+    
     for (int i = 0; i < strlen(word); i++)
     {
         wordarray[strlen(word)] = {}
