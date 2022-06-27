568a63e3345cdb871a4c7380a386fd4f8d428142
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-27 21:03:30 +0000

    Mon, Jun 27, 2022, 9:03 PM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -31,7 +31,7 @@ int letters(string text)
     int letters = 0;
     for (int i = 0; i < strlen(text); i++)
     {
-        if (64 < text[i] < 91) // 'A' 'z'
+        if ('A' <= text[i] <= 'z') // 'A' 'z'
         {
             letters++;
         }
