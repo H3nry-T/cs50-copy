8286e0e893ddc789c0b307ea2c05eef8ccc7250e
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-27 19:22:37 +0000

    Mon, Jun 27, 2022, 7:22 PM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -28,3 +28,5 @@ int grade(string text)
 // index = 0.0588 * L - 0.296 * S - 15.8
 //where L is the average number of letters per 100 words in the text,
 //and S is the average number of sentences per 100 words in the text.
+//Congratulations! Today is your day. You're off to Great Places! You're off and away!
+//
