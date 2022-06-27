1c6618a9582cb7cc5bdc5f8e7076a9ef8690f339
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-27 22:09:10 +0000

    Mon, Jun 27, 2022, 10:09 PM +00:00

---
+++ pset2/readability/read.c
@@ -0,0 +1,56 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+int letters(string text);
+int sentences(string text);
+int words(string text);
+
+int main(void)
+{
+    string text = get_string("TEXT:\n");
+
+    int letter = letters(text);
+    printf("this text has %i", letter);
+}
+
+int letters(string text)
+{
+    int letters = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (isalpha(text[i])) // 'A' 'z'
+        {
+            letters++;
+        }
+    }
+    return letters;
+}
+int sentences(string text)
+{
+    int sentences = 0;
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') // '.' '!' '?'
+        {
+            sentences++;
+        }
+    }
+    return sentences;
+}
+int words(string text)
+{
+    int words = 0;
+    int i = 0;
+    while (text[i] != '\0') //chraracter is not NULL
+    {
+        if (text[i] == ' ' || text[i] == '\n' || text[i] == '\t')
+        {
+            words++;
+        }
+        i++;
+    }
+    return words;
+}
