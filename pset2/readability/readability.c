f596d391fe307048ebe6877ce44307dce7b5e7e6
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-27 19:21:19 +0000

    Mon, Jun 27, 2022, 7:21 PM +00:00

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -25,5 +25,6 @@ int grade(string text)
 }
 // longer words
 // longer sentences
-// index = o.0588 * L = 0.296 * S - 15.8
-//where L is the average number of letters per 100 words in the text, and S is the average number of sentences per 100 words in the text.
+// index = 0.0588 * L - 0.296 * S - 15.8
+//where L is the average number of letters per 100 words in the text,
+//and S is the average number of sentences per 100 words in the text.
