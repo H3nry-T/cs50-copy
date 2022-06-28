34ce052270a239d88bde893e042b09ac87ff168d
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-28 16:38:02 +0000

    Tue, Jun 28, 2022, 4:38 PM +00:00

--- pset2/caesar/c.c
+++ pset2/caesar/c.c
@@ -42,7 +42,7 @@ bool only_digits(string key) // if key = 20x this should return false
 
 char rotate(char c, int key)
 {
-    char cipher = 0;
+    char cipher;
     if(isupper(c))
     {
         char alphaindex = c - 'A'; //alphaindex for A should be 0
