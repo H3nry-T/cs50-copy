7145ccdfc30e5fd57eeb450fc81bff2e83a7f5cc
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-28 19:15:50 +0000

    Tue, Jun 28, 2022, 7:15 PM +00:00

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -9,7 +9,7 @@ int main(int argc, string argv[])
     //single command line argument
     //case insensitive
     //if no argument or too many argument return 1;
-    if (int argc < 2 || int argc > 2 || argv[1] != 26 || isalpha(argv[1]) == false || )
+    if (int argc != 2 || strlen(argv[1]) != 26 || isalpha(argv[1]) == false || )
     {
         printf("ERROR\n");
         return 1;
