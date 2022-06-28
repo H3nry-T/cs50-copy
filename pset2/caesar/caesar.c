b4031fbc63b32467fe7eb4a969a033e6470efc34
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-28 12:28:37 +0000

    Tue, Jun 28, 2022, 12:28 PM +00:00

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -6,10 +6,19 @@ int main(int argc, string argv[])
     // take in a command line argument for a key
     // if the user does not give a number
     // if the user does not give a argument
+    if (argc == 1)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
     // if the user gives multiple arguments
+    else if(argc > 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
     //print the Usage: ./caesar key
     // convert the key given into an integer
-    int key = argv[1];
     // prompt the user for a plain text
     // rotate the characters but do not change any punctuation
     // output the cipher text
