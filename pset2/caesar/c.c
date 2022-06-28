217aa7de7be897fb42786fd7f407e417c3eb9603
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-28 14:22:12 +0000

    Tue, Jun 28, 2022, 2:22 PM +00:00

--- pset2/caesar/c.c
+++ pset2/caesar/c.c
@@ -1,15 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+bool only_digits(string key);
 
 int main(int argc, string argv[])
 {
-    printf("argv[last] is %s\n", argv[argc - 1]);
-    printf("argv[first] is %s\n", argv[1]);
+    if (only_digits(argv[1]))
+    {
+        printf("key is digits only\n");
+    }
+    else
+    {
+        printf("key has a non digit in it\n");
+    }
 }
-// take in a command line argument for a key
-// if the user does not give a number
-// if the user does not give a argument
-// if the user gives multiple arguments
-//print the Usage: ./caesar key
-// k (secret key) has to be a positive number
-// preserve upper and lower case letters when printing ciphertext
+
+bool only_digits(string key) // if key = 20x this should return false
+{
+    //turn string into characters
+    int i = 0;
+    while(i < strlen(key))
+    {
+        if (isdigit(key[i]) != 0) // if char is digit
+        {
+            i++;
+        }
+        else
+        {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
