685fe36eb5074a8c5cbed34373e671973552e521
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-28 12:49:18 +0000

    Tue, Jun 28, 2022, 12:49 PM +00:00

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -7,7 +7,7 @@ int main(int argc, string argv[])
 {
 
     // if the user does not give a argument or gives too many arguments
-    if (argc == 1 || argc > 2 || only_digits(argv[1]))
+    if (argc == 1 || argc > 2 || only_digits(argv[1]) == 1)
     {
         printf("Usage: ./caesar key\n");
         return 1;
@@ -16,18 +16,19 @@ int main(int argc, string argv[])
     {
         return 0;
     }
-    // if the user gives multiple arguments
-    //print the Usage: ./caesar key
+
     // convert the key given into an integer
     // prompt the user for a plain text
     // rotate the characters but do not change any punctuation
     // output the cipher text
 }
-// take in a command line argument for a key
-// if the user does not give a number
-// if the user does not give a argument
-// if the user gives multiple arguments
-//print the Usage: ./caesar key
+
+bool only_digits(key)
+{
+    
+}
+
+
 // k (secret key) has to be a positive number
 // preserve upper and lower case letters when printing ciphertext
 // if k > 26 keep rotating through the ascii codes. so 27 more than A is B NOT [
