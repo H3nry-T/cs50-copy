619c8ac7494b387aa2b16e6a5e39b9dffd36d759
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-28 13:28:58 +0000

    Tue, Jun 28, 2022, 1:28 PM +00:00

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -29,15 +29,15 @@ bool only_digits(string key)
     //turn string into characters
     for (i = 0; i < strlen(key); i++)
     {
-        digit[] += key[i]
-    }
-    if (isdigit(digit[i]) != 0) // if decimal digit return true
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
+        char digit[i] = key[i]
+        if (isdigit(digit[i]) != 0) // if decimal digit return true
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
     }
 }
 
