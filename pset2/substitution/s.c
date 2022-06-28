5ea5a226343b663bdcd44225a318b47034e910d3
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-28 23:37:11 +0000

    Tue, Jun 28, 2022, 11:37 PM +00:00

--- pset2/substitution/s.c
+++ pset2/substitution/s.c
@@ -42,3 +42,40 @@ bool salphacheck(string key)
     }
     return true;
 }
+
+//debug
+char sub(string ptext, string key) //take plaintext and key and output subbed character
+{
+    for (int i = 0; i < strlen(ptext); i++) //loop through ptext
+    {
+        if(isupper(ptext[i])) //sub all uppercase characters with the key
+        {
+            int alphaindex = ptext[i] - 'A';//take uppercase character - 65
+            for (int j = 0; j < 27; j++) // HERE DOWN REPLACE
+            {
+                if(alphaindex == j)//compare the keyindex (j) to the uppercase letter
+                {
+                    ptext[i] = key[j];//replace e.g letter A with key[0]
+                    return ptext[i];
+                }
+            }
+        }
+        else if (islower(ptext[i]))// if lower case
+        {
+            int alphaindex = ptext[i] - 'a';//lowercase letter - 91
+            for (int j = 0; j < 26; j++)
+            {
+                if(alphaindex == j)//compare keyindex (j) to the lowercase letter
+                {
+                    ptext[i] = key[j];//replace e.g letter a with key[0]
+                    return ptext[i];
+                }
+            }
+        }
+    }
+    return 0; //exit function if not upper,lower case alphabet
+}
+
+//replace with
+
+ptext[i] = key[alphaindex]
