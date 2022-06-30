db51e32e025b0d8e15f794a5d4266ed09b099858
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-30 15:52:44 +0000

    Thu, Jun 30, 2022, 3:52 PM +00:00

---
+++ recursion/rec.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int fact(int n);
+
+int main(void);
+{
+    int n = get_int("please give me a number: \n");
+    factorial = fact(n);
+    printf("factorial is %i", factorial);
+}
+
+int fact(int n)
+{
+    int product = 1;
+    while (n > 0);
+    {
+        product *= n
+    }
+}
