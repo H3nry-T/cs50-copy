4f1471e6117a408e9de506134088e1121c8e0fa7
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-30 11:39:27 +0000

    Thu, Jun 30, 2022, 11:39 AM +00:00

--- pset1/mario-less/m.c
+++ pset1/mario-less/m.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void pyramid(int height);
+
 int main(void)
 {
     int n = get_int("what is the height?: ");
-    
+
+}
+
+void pyramid(int height)
+{
+    printf(height); 
+    pyramid(height - 1);
 }
