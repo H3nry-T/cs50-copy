06612fca469a7cf03541dea753ee3031bdd8bfa5
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-30 15:56:29 +0000

    Thu, Jun 30, 2022, 3:56 PM +00:00

--- recursion/rec.c
+++ recursion/rec.c
@@ -3,17 +3,17 @@
 
 int fact(int n);
 
-int main(void);
+int main(void)
 {
-    int n = get_int("please give me a number: \n");
-    factorial = fact(n);
-    printf("factorial is %i", factorial);
+    int n = get_int("please give me a number: ");
+    int factorial = fact(n);
+    printf("factorial is %i\n", factorial);
 }
 
 int fact(int n)
 {
     int product = 1;
-    while (n > 0);
+    while (n > 0)
     {
         product *= n;
         n--;
