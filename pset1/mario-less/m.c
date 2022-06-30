8ff8e23c1f8a0c38a4c087f13cfdc420fc647edb
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-30 12:00:54 +0000

    Thu, Jun 30, 2022, 12:00 PM +00:00

--- pset1/mario-less/m.c
+++ pset1/mario-less/m.c
@@ -11,12 +11,11 @@ int main(void)
 
 void pyramid(int n)
 {
+    pyramid(n - 1);
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+        printf("#"); 
     }
+
 }
