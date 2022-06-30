f7d612305cea5c27c77c8a80e6eb9d439e17028e
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-06-30 16:13:27 +0000

    Thu, Jun 30, 2022, 4:13 PM +00:00

--- recursion/collatz.c
+++ recursion/collatz.c
@@ -19,20 +19,16 @@ int main(void)
 
 int collatz(int n)
 {
-    int count = 0;
     if (n == 1)
     {
         return 0;
     }
     else if ((n % 2) == 0)
     {
-        collatz(n/2);
-        count++;
+        return 1 + collatz(n/2);
     }
     else
     {
-        collatz(3 * n + 1);
-        count++;
+        return 1 + collatz(3 * n + 1);
     }
-    return count;
 }
