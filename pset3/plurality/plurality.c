76fd547efd0a17fee7f975ecc2397d3eaf620283
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-01 13:57:43 +0000

    Fri, Jul 1, 2022, 1:57 PM +00:00

--- pset3/plurality/plurality.c
+++ pset3/plurality/plurality.c
@@ -79,7 +79,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    string winner[candidate_count] = candidates[0].name;
+    string winner[candidate_count] = 0:
     int scorewin = candidates[0].votes;
     for (int i = 0; i < candidate_count; i++)
     {
