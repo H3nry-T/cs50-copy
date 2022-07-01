20df1783c28f14a9af6ad2938f1c7bab50b7bf17
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-01 19:35:26 +0000

    Fri, Jul 1, 2022, 7:35 PM +00:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -129,7 +129,14 @@ bool vote(int voter, int rank, string name)
 {
     for()
     {
-        if (strcmp(name, candidates[i].name))
+        for (i = 0; i < candidate_count; i++)
+        {
+            if (strcmp(name, candidates[i].name) == 0)
+            {
+                candidates[i].votes += 1;
+                return true;
+            }
+        }
     }
     return false;
 }
