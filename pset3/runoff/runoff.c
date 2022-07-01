af4a87a23df42530d62d469226556acab45106d0
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-01 21:53:09 +0000

    Fri, Jul 1, 2022, 9:53 PM +00:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -195,7 +195,7 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for (int i = 0; )
     return false;
 }
 
