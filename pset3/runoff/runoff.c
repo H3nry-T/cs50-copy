3e16d59efedb353ad18a66f2024d3fbbed3cdf95
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-01 22:13:50 +0000

    Fri, Jul 1, 2022, 10:13 PM +00:00

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -219,6 +219,9 @@ void eliminate(int min)
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
     }
     return;
 }
