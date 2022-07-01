cba951260199fbec28fb7fdc3a38af637bf68741
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-01 15:17:42 +0000

    Fri, Jul 1, 2022, 3:17 PM +00:00

--- pset3/plurality/plurality.c
+++ pset3/plurality/plurality.c
@@ -84,7 +84,6 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        
         if (candidates[i].votes > candidates[0].votes)
         {
             winner[0] = candidates[i].name;
