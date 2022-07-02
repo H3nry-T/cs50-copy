d1c120a968e6efc3f780083490be6b56ea73f33f
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-02 18:20:05 +0000

    Sat, Jul 2, 2022, 6:20 PM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -118,7 +118,7 @@ void record_preferences(int ranks[])
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            preferences[ranks[i]][ranks[j]] += 1
+            preferences[ranks[i]][ranks[j]] += 1;
         }
     }
     return;
