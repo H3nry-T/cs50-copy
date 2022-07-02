e6f7bf68a61813a90356a4fcf224c20816b1ccb9
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-02 14:03:26 +0000

    Sat, Jul 2, 2022, 2:03 PM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -100,7 +100,13 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    if ()
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            
+        }
+    }
     return false;
 }
 
