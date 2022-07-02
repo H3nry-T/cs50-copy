180385cac1573145797325a766e77d3b4d14ab05
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-02 21:19:23 +0000

    Sat, Jul 2, 2022, 9:19 PM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -134,7 +134,7 @@ void add_pairs(void)
             if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
-                pairs[pair_count].loser = preferences[j][i];
+                pairs[pair_count].loser = j;
                 pair_count++;
             }
             else if (preferences[i][j] < preferences[j][i])
