7a807c397dc31d837c54f08e3d557e0a30312605
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-03 23:29:10 +0000

    Sun, Jul 3, 2022, 11:29 PM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -166,11 +166,10 @@ void sort_pairs(void)
         int higheststrength_index = i;
         for (int j = i + 1; j < pair_count - 1; j++)
         {
-            int newhighest_maxv = victorystgh(j);
             if (newhighest_maxv > maxv)
             {
+                int maxv = victorystgh(j);
                 higheststrength_index = j;
-                maxv = newhighest_maxv;
             }
         }
         if (higheststrength_index != i)
