bb564d33b33c82b27e8a4bc4ee28ad269be0d37a
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-03 00:14:47 +0000

    Sun, Jul 3, 2022, 12:14 AM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -152,7 +152,7 @@ void sort_pairs(void)
         int begin_index = i; //selection sort starting position
         for (int j = i - 1; j >= 0; j--)
         {
-            if (preferences[pairs[j].winner][pairs[j].loser] < stgh_v)
+            if (preferences[pairs[j].winner][pairs[j].loser] < stgh_v)//test each subsequent strength with 
             {
                 minv = preferences[pairs[j].winner][pairs[j].loser];
                 int min_index = j;
