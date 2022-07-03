7ece09881d75c4433eb4f769d5d8e55383297033
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-03 22:40:38 +0000

    Sun, Jul 3, 2022, 10:40 PM +00:00

--- pset3/tideman/t.c
+++ pset3/tideman/t.c
@@ -157,8 +157,11 @@ void sort_pairs(void)
 {
     for (int i = 0; i < pair_count - 1; i++)
     {
-        int max_v = preferences[pairs[i].winner][pairs[j].loser] - preferences[pairs[j].winner][pairs[i].loser];
+        int max_v = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].winner][pairs[i].loser];
         for (int j = i + 1; j < pair_count - 1; j++)
+        {
+            if ()
+        }
     }
     return;
 }
