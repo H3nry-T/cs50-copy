e1612cc4e4d49a4ec4a9e5a5218dee9e29eda20a
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-03 23:13:25 +0000

    Sun, Jul 3, 2022, 11:13 PM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -159,6 +159,7 @@ void sort_pairs(void)
     for (int i = 0; i < pair_count - 1; i++)
     {
         maxv = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].winner][pairs[i].loser];
+        higheststrength_index = i;
         for (int j = i + 1; j < pair_count - 1; j++)
         {
             newhighest_maxv = preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].winner][pairs[j].loser];
