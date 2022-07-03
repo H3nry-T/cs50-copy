8ac58b7b5d1a2ba949016eb62d1e25b8629429bc
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-03 00:45:51 +0000

    Sun, Jul 3, 2022, 12:45 AM +00:00

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -202,6 +202,19 @@ void lock_pairs(void)
 // Print the winner of the election
 void print_winner(void)
 {
-    // TODO
+    for (int row = 0; row < candidate_count; row++)
+    {
+        for (int col = 0; col < candidate_countj; col++)
+        {
+            if(locked[row][col] == true)
+            {
+                break;
+            }
+            else if (col == candidate - 1)
+            {
+                printf("%s", candidates[row]);
+            }
+        }
+    }
     return;
 }
