0215f3b69056205305a47a382720072f630b585e
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-06 22:27:50 +0000

    Wed, Jul 6, 2022, 10:27 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -10,8 +10,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             float B = image[i][j].rgbtBlue * 255;
             float G = image[i][j].rgbtGreen * 255;
             float R = image[i][j].rgbtRed * 255;
-            int average =round((R + G + B) / 3 );
-            
+            int avrgcolour =round((B + G + R) / 3 );
+            B = avrgcolour;
+            G = avrgcolour;
+            R = avrgcolour; 
         }
     }
     return;
