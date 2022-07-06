439bfba89c326f2be5ec9bb20cc0f32aa25b36f6
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-06 19:26:42 +0000

    Wed, Jul 6, 2022, 7:26 PM +00:00

---
+++ pset1/cash/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char* string;
+
+
+
+int main(void)
+{
+    int x;
+    printf("pls give integer: ");
+    scanf("%i\n", &x);
+    printf("the number is %i\n", x);
+    return 0;
+}
