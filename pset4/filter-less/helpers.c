92be3c82a53ac56d8b0f7a18d81e81021c446c30
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-07 14:46:41 +0000

    Thu, Jul 7, 2022, 2:46 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -80,6 +80,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             copy[i][j].rgbtRed = mean(image[i - 1][j - 1].rgbtRed, image[i - 1][j].rgbtRed, image[i - 1][j + 1].rgbtRed, image[i][j - 1].rgbtRed, image[i][j].rgbtRed, image[i][j + 1].rgbtRed, image[i + 1][j - 1].rgbtRed, image[i + 1][j].rgbtRed, image[i + 1][j + 1].rgbtRed)
             copy[i][j].rgbtBlue = mean(image[i - 1][j - 1].rgbtGreen, image[i - 1][j].rgbtGreen, image[i - 1][j + 1].rgbtGreen, image[i][j - 1].rgbtGreen, image[i][j].rgbtGreen, image[i][j + 1].rgbtGreen, image[i + 1][j - 1].rgbtGreen, image[i + 1][j].rgbtGreen, image[i + 1][j + 1].rgbtGreen)
             copy[i][j].rgbtGreen = mean(image[i - 1][j - 1].rgbtBlue, image[i - 1][j].rgbtBlue, image[i - 1][j + 1].rgbtBlue, image[i][j - 1].rgbtBlue, image[i][j].rgbtBlue, image[i][j + 1].rgbtBlue, image[i + 1][j - 1].rgbtBlue, image[i + 1][j].rgbtBlue, image[i + 1][j + 1].rgbtBlue)
+
         }
     }
 
