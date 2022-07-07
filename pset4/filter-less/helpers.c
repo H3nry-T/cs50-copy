86e5c11971aea2829538adc8fbe8bce0d164fb89
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-07 16:57:10 +0000

    Thu, Jul 7, 2022, 4:57 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -78,26 +78,23 @@ centre(RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE copy[height][width];
+    RGBTRIPLE copy[height][width]; //make empty copy of the image bitmap.
+
+    for (int i = 0; i < height; i++) //loop through the image and copy it.
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
+    
+    // setup a counter for red, green, blue, for 3 x 3 grid.
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE initial_colour = {0, 0, 0};
-            copy[i][j] = initial_colour;
-            if (centre() == true)
-            {
-                
-            }
-            if (edge() == true)
-            {
 
-            }
-            if (corner() == true)
-            {
-
-            }
         }
     }
     return;
