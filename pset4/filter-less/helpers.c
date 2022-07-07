090478c0c4642cca1dc5c9d14702a52a99e5313b
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-07 10:33:57 +0000

    Thu, Jul 7, 2022, 10:33 AM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -62,7 +62,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        swap();
+        swap(image[i][j]);
     }
     return;
 }
