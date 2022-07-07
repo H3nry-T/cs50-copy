c7f20e39b9f9acb6750bdb373f067c9d74155f94
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-07 20:49:05 +0000

    Thu, Jul 7, 2022, 8:49 PM +00:00

--- pset4/filter-less/bmp.h
+++ pset4/filter-less/bmp.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 
-// These data types are essentially aliases for C/C++ primitive data types. 
+// These data types are essentially aliases for C/C++ primitive data types.
 // Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
 // See https://en.wikipedia.org/wiki/C_data_types#stdint.h for more on stdint.h.
 
@@ -55,3 +55,4 @@ typedef struct
     BYTE  rgbtRed;
 } __attribute__((__packed__))
 RGBTRIPLE;
+
