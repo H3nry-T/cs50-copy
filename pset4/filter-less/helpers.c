ae476a913caaf979a930378e33377164b8733bf6
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-07 15:11:34 +0000

    Thu, Jul 7, 2022, 3:11 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -76,7 +76,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE copy[i][j] = image[i][j];
+            RGBTRIPLE copy[i][j] = {0, 0, 0};
+            copy[i][j] = image[i][j];
             copy[i][j].rgbtRed = (image[i - 1][j - 1].rgbtRed+ image[i - 1][j].rgbtRed+ image[i - 1][j + 1].rgbtRed+ image[i][j - 1].rgbtRed+ image[i][j].rgbtRed+ image[i][j + 1].rgbtRed+ image[i + 1][j - 1].rgbtRed+ image[i + 1][j].rgbtRed+ image[i + 1][j + 1].rgbtRed) / 9;
             copy[i][j].rgbtBlue = (image[i - 1][j - 1].rgbtGreen+ image[i - 1][j].rgbtGreen+ image[i - 1][j + 1].rgbtGreen+ image[i][j - 1].rgbtGreen+ image[i][j].rgbtGreen+ image[i][j + 1].rgbtGreen+ image[i + 1][j - 1].rgbtGreen+ image[i + 1][j].rgbtGreen+ image[i + 1][j + 1].rgbtGreen) / 9;
             copy[i][j].rgbtGreen = (image[i - 1][j - 1].rgbtBlue+ image[i - 1][j].rgbtBlue+ image[i - 1][j + 1].rgbtBlue+ image[i][j - 1].rgbtBlue+ image[i][j].rgbtBlue+ image[i][j + 1].rgbtBlue+ image[i + 1][j - 1].rgbtBlue+ image[i + 1][j].rgbtBlue+ image[i + 1][j + 1].rgbtBlue) / 9;
