bdaa478ee8c6c943d229780a99d21812291432a5
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-07 20:38:50 +0000

    Thu, Jul 7, 2022, 8:38 PM +00:00

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -102,11 +102,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     int gridscanindex_x = j + c;
                     if (gridscanindex_y < 0 || gridscanindex_x < 0 || gridscanindex_x > width - 1 || gridscanindex_y > height - 1)
                     {
-                        redtotal += image[gridscanindex_y][gridscanindex_x].rgbtRed;
-                        greentotal += image[gridscanindex_y][gridscanindex_x].rgbtGreen;
-                        bluetotal += image[gridscanindex_y][gridscanindex_x].rgbtBlue;
-                        count++;
+                        continue; 
                     }
+                    redtotal += image[gridscanindex_y][gridscanindex_x].rgbtRed;
+                    greentotal += image[gridscanindex_y][gridscanindex_x].rgbtGreen;
+                    bluetotal += image[gridscanindex_y][gridscanindex_x].rgbtBlue;
+                    count++;
                 }
             }
             BYTE blurpixelred = round (redtotal / count);
