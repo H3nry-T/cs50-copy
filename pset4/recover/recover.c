ee6d056e3ae11524a1d83dbed3c92bcc98f051c1
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-08 13:08:14 +0000

    Fri, Jul 8, 2022, 1:08 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -3,5 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-     argv[] 
+    if (argc != 2)
+    {
+        printf("only accepts one command-line argument\n");
+        printf("Correct usage: <name of forensic image> \n");
+        return 1;
+    }
+    FILE *f = fopen(filename, "r"); 
+    fread()
 }
