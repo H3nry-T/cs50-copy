905acaa1b83f136124bf81c39a78e2e47713ae8d
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-08 20:44:56 +0000

    Fri, Jul 8, 2022, 8:44 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -35,10 +35,10 @@ int main(int argc, char *argv[])
             //if first jpeg? yes
             if (jpegcount == 0)
             {
+                jpegcount++;
                 //recover the file and name "###.jpg" and write first 512 bytes.
                 sprintf(filename, "%03i.jpg", jpegcount);
                 img = fopen(filename, "w");
-                jpegcount++;
                 fwrite(buffer, 1, 512, img);
             }
             //else we find new jpeg?
