b5193f45093daf2db3c9622082e9efa013679afd
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-08 15:08:23 +0000

    Fri, Jul 8, 2022, 3:08 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -21,7 +21,9 @@ int main(int argc, char *argv[])
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
         jpegcount++;
-        char *filenameptr = "
+        char *filename[8];
         sprintf(filename, "%03i.jpg", jpegcount);
+        FILE *img = fopen(filename, "w");
+        
     }
 }
