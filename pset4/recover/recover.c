fc1863362ffb72e4b938498996987062e3f14802
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-08 18:19:11 +0000

    Fri, Jul 8, 2022, 6:19 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
         printf("Correct usage: <name of forensic image> \n");
         return 1;
     }
-    FILE *f = fopen(argv[1], "r"); //get the file pointer which is the memory address of the opened image.
+    FILE *f = fopen(argv[1], "r"); //open card file
     if (f == NULL)
     {
         printf("the file does not exist\n");
@@ -17,13 +17,14 @@ int main(int argc, char *argv[])
     }
 
     int *buffer = malloc(512);
-    while (fread(buffer, 1, 512, f) == 512) //keep reading changes if it reaches the end.
+    while (fread(buffer, 1, 512, f) == 512) //repeat until end of card
     {
-        // f is the card.raw file
+        // read card.raw 512 bytes into buffer
         fread(buffer, 512, 1, f);
         int jpegcount = 0;
-
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)// if jpeg.
+        
+        //start of new jpeg?
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             if (jpegcount == 0) //first jpeg
             {
