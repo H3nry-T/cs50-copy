a4f962d716fff38b509bd3c5653d195ead22a769
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-08 20:41:08 +0000

    Fri, Jul 8, 2022, 8:41 PM +00:00

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    BYTE *buffer = malloc(512);
+    BYTE buffer[512];
     int jpegcount = 0;
     FILE *img = NULL;
     char *filename = malloc(8);
@@ -64,6 +64,5 @@ int main(int argc, char *argv[])
     fclose(f);
     fclose(img);
     free(filename);
-    free(buffer);
     return 0;
 }
