ddfefc4bada1c2523c30657cae6a538f854a07e0
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-09 16:49:47 +0000

    Sat, Jul 9, 2022, 4:49 PM +00:00

--- test/linkedlist.c
+++ test/linkedlist.c
@@ -4,19 +4,41 @@
 typedef struct node
 {
     int number;
-    node *pointer;
+    node *nextnode;
 }
 node;
 
 
 int main(void)
 {
+    //create a node for my linked list
     node *list = NULL;
 
+    //create  temporary node that is empty in RAM
     node *n = malloc(sizeof(node));
     if (n == NULL)
     {
         return 1;
     }
-    
+
+    //initialise our first node
+    n -> number = 1;
+    n -> nextnode = NULL;
+
+    // update list to point to the temporary node
+
+    list = n;
+
+    //add a number to the list
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    n -> number = 2;
+    n -> next = NULL;
+
+    list -> next = n;
 }
