52eb1fc6b55e7bbfe4bcaeb90c53c622162b695d
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-13 11:30:26 +0000

    Wed, Jul 13, 2022, 11:30 AM +00:00

--- pset5/inheritance/inheritance.c
+++ pset5/inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *newperson = malloc(sizeof(person));
+    person *p = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -50,23 +50,19 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        newperson -> parents[0] = parent0;
-        newperson -> parents[1] = parent1;
+        p -> parent[0] = parent0;
+        p -> parent[1] = parent1; 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        newperson -> alleles[0] = parent0 -> alleles[rand() % 2];
-        newperson -> alleles[1] = parent1 -> alleles[rand() % 2];
+
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        newperson -> parents[0] = NULL;
-        newperson -> parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        newperson -> alleles[0] = random_allele();
-        newperson -> alleles[1] = random_allele();
+
     }
 
     // TODO: Return newly created person
@@ -77,15 +73,11 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p == NULL)
-    {
-        return;
-    }
+
     // TODO: Free parents recursively
-    free_family(p -> parents[0]);
-    free_family(p -> parents[1]);
+
     // TODO: Free child
-    free(p);
+
 }
 
 // Print each family member and their alleles.
