ad868723531f722bd950ec75a918eacf22a13c39
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-13 10:37:14 +0000

    Wed, Jul 13, 2022, 10:37 AM +00:00

--- pset5/inheritance/inheritance.c
+++ pset5/inheritance/inheritance.c
@@ -56,8 +56,8 @@ person *create_family(int generations)
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
         int x = rand() % 2;
         int y = rand() % 2;
-        newperson -> alleles[0] = parent0 -> alleles[x];
-        newperson -> alleles[1] = parent1 -> alleles[y];
+        newperson -> alleles[0] = newperson -> parents[0] -> alleles[x];
+        newperson -> alleles[1] = newperson -> parents[1] -> alleles[y];
     }
 
     // If there are no generations left to create
