5fd1979b178ccfe45c9b3e6c4aaeea60f1f1b4ef
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-19 20:07:12 +0000

    Tue, Jul 19, 2022, 8:07 PM +00:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -21,7 +21,7 @@ node *table[N];
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    fopen()
+    //TODO
     return false;
 }
 
@@ -35,7 +35,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODOj
+    FILE *file = fopen()
     return false;
 }
 
