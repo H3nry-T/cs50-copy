d627d2e13bf8e7f76288af380f6d19260008faf8
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-21 09:00:02 +0000

    Thu, Jul 21, 2022, 9:00 AM +00:00

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -96,7 +96,15 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    return wordcount;
+    if (wordcount > 0)
+    {
+        return wordcount;
+    }
+    else
+    {
+        return 0;
+
+    }
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
