40fe88ddd21e40c1084a17e76d6e81607a836dd4
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-23 16:00:37 +0000

    Sat, Jul 23, 2022, 4:00 PM +00:00

--- test/python.py
+++ test/python.py
@@ -1,9 +1,13 @@
 #if statmeent
 counter = 0
-while (counter <= 10):
+while (counter < 1):
     print(f"counter: {counter}")
     counter += 1
 
-for i in range(0,11,1):
+for i in range(0,1,1):
     print(f"counter: {i}")
 
+nums = [n for n in range(10)]
+nums += [4]
+for n in nums:
+    print(n)
