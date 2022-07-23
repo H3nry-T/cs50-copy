e944baa74cf86215a60a7f6c62bc2ad46703e2b3
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-23 15:50:43 +0000

    Sat, Jul 23, 2022, 3:50 PM +00:00

--- test/python.py
+++ test/python.py
@@ -1,8 +1,8 @@
 #if statmeent
 counter = 0
-while (counter < 100):
-    print(f"{counter}", end = 0)
+while (counter <= 10):
+    print(f"counter: {counter}")
     counter += 1
 
-    
-
+for i in range(0,11,1):
+    print(f"counter: {i}")
