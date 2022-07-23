113ea62facd39f4ab1062df5fd8a8757d9e352d0
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-23 19:34:22 +0000

    Sat, Jul 23, 2022, 7:34 PM +00:00

--- test/mario.py
+++ test/mario.py
@@ -1,12 +1,13 @@
 from cs50 import get_int
 
-while True:
-    n = get_int("please input a number:")
-    if n > 0:
-        break
+def main():
+    height = getheight()
+    for i in range(n)
+        print("#")
 
-
-
-for i in range(n)
-    print("#")
-
+def getheight():
+    while True:
+        n = get_int("please input a number:")
+        if n > 0:
+            break
+    return n
