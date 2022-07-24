30fa10ee5c6c867bdda15a5f15bd34933b3ae054
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-24 17:25:01 +0000

    Sun, Jul 24, 2022, 5:25 PM +00:00

--- pset6/sentimental-mario-more/mario.py
+++ pset6/sentimental-mario-more/mario.py
@@ -1,1 +1,9 @@
-# TODO
+from cs50 import get_int
+
+while True:
+    height = get_int("input a height:")
+    if 0 < height < 9:
+        break
+    else:
+        print("input a number between 1 and 8 inclusive")
+        continue
