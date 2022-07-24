3128622508fee113270b20710170139da0aeb568
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-24 15:44:41 +0000

    Sun, Jul 24, 2022, 3:44 PM +00:00

--- pset6/world-cup/tournament.py
+++ pset6/world-cup/tournament.py
@@ -65,7 +65,8 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # TODO
-    for i in range(math.log2(len(teams))):
+    divbytwo = math.log2(len(teams))
+    for i in range(int(divbytwo)):
         teams = simulate_round(teams)
     return teams[0]["team"]
 
