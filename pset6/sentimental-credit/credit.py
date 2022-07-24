d44b15f80e325036f7a69c65e2dbc8e2a232210c
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-24 20:49:26 +0000

    Sun, Jul 24, 2022, 8:49 PM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -3,12 +3,13 @@ from cs50 import get_string
 
 def main():
     while True:
-        number = get_int("please type in your credit card number:\n") 
+        number = get_int("please type in your credit card number:\n")
         if number > 0:
             break
         else:
             print("please insert a valid credit card number")
             continue
+    
 
 if __name__ == "__main__":
     main()
