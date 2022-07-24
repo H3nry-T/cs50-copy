e47d010932fe955be5aab45b1407a728ff60290d
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-24 17:59:18 +0000

    Sun, Jul 24, 2022, 5:59 PM +00:00

--- pset6/sentimental-cash/cash.py
+++ pset6/sentimental-cash/cash.py
@@ -1,6 +1,13 @@
 from cs50 import get_float
 
 def main():
+    while True:
+        change = get_float("please input your change\n")
+        if 0 < change:
+            break
+        else:
+            print("please input a positive number\n")
+            continue
 
 
 
@@ -12,8 +19,13 @@ def main():
 
 
 
+def calculate_quarters(change):
 
+def calculate_dimes(change):
 
+def calculate_nickels(change):
+
+def calculate_pennies(change):
 
 
 
