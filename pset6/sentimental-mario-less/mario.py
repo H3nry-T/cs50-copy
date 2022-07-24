6def0f00e94ac273d2c0f5a0f4fc732658413530
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-24 17:09:57 +0000

    Sun, Jul 24, 2022, 5:09 PM +00:00

--- pset6/sentimental-mario-less/mario.py
+++ pset6/sentimental-mario-less/mario.py
@@ -2,11 +2,11 @@ from cs50 import get_int
 
 while True:
     height = get_int("input the pyramid height:\n")
-    if 0 < x < 9:
+    if 0 < height < 9:
         break
 
 for i in range(height):
-    for j in range(height - (i + 1)):
+    for j in range(height):
         print(".", end = "")
 
     print("")
