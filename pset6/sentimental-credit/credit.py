74b9fd06f5d9b5e8505ba69ef414a1c53df1e41f
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-25 09:04:08 +0000

    Mon, Jul 25, 2022, 9:04 AM +00:00

--- pset6/sentimental-credit/credit.py
+++ pset6/sentimental-credit/credit.py
@@ -27,7 +27,7 @@ def main():
 
     productdigits = []
     for i in range(len(digitarray)):
-        for j in range(lengthnumber(int(digitarray[i])) + 1):
+        for j in range():
             productdigit = nthlastdigit(digitarray[i], j)
             productdigits.append(productdigit)
 
