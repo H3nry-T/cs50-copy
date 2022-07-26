ccd02e00a180d1da631de70204f6519ef0f305a1
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-26 15:39:37 +0000

    Tue, Jul 26, 2022, 3:39 PM +00:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("need a csv file and a txt file")
         sys.exit()
     # TODO: Read database file into a variable
