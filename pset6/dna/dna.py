879f2116e3213a83fee0cacc32e302895e1ca15e
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-26 18:20:41 +0000

    Tue, Jul 26, 2022, 6:20 PM +00:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -11,7 +11,7 @@ def main():
 
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1]) as database:
+    with open(sys.argv[1], "r") as database:
         database_dictreader = csv.DictReader(database)
         for row in database_dictreader:
             print(row)
@@ -22,7 +22,7 @@ def main():
 
 
     # TODO: Read DNA sequence file into a variable
-    with open(sys.argv[2]) as txtfile:
+    with open(sys.argv[2], "r") as txtfile:
         txtfile_reader = csv.reader(txtfile)
         for row in txtfile_reader:
             print(row)
