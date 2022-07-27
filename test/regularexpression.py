e3913d848ac75427d387343e1874eb68156b37de
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-27 18:25:47 +0000

    Wed, Jul 27, 2022, 6:25 PM +00:00

--- test/regularexpression.py
+++ test/regularexpression.py
@@ -1,10 +1,16 @@
 import re
 import cs50
 import sys
+
 while True:
     input = cs50.get_string("pls type in a string: ")
+    print("")
+
     if input.isdigit():
-        print("this seems to be a number")
+        print("this seems to be a number\n")
+        continue
+    elif len(input) <= 0:
+        print("pls type something in\n")
         continue
     else:
         break
@@ -13,3 +19,5 @@ formatted_input = input.strip().lower()
 
 if re.search("^(hello)$", formatted_input):
     print("it matches!")
+else:
+    print("it doesn't match")
