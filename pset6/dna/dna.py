f788409ad271ab3a8eba318d6a8a6d180671e7db
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-27 09:08:08 +0000

    Wed, Jul 27, 2022, 9:08 AM +00:00

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -29,7 +29,7 @@ def main():
     # TODO: Find longest match of each STR in DNA sequence
     longestruns = []
     for i in range(len(subsequences)):
-        print(longest_match(next(txtfile_reader), subsequences[i]))
+        print(longest_match(next(txtfile_reader), ))
 
 
     # TODO: Check database for matching profiles
