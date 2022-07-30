806c7b1233db809d92ecddbddc7f7d126e0e4336
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-30 18:40:04 +0000

    Sat, Jul 30, 2022, 6:40 PM +00:00

--- pset7/movies/6.sql
+++ pset7/movies/6.sql
@@ -1,2 +1,4 @@
 SELECT title, year FROM movies WHERE year = 2012;
 SELECT AVG(rating) FROM ratings WHERE movie_id
+
+SELECT AVG(rating) FROM ratings WHERE movie_id = (SELECT id FROM movies WHERE year = 2012);
