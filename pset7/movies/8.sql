1d90fc8e7295d61af9fd33663e0aafe00cc37103
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-30 19:22:09 +0000

    Sat, Jul 30, 2022, 7:22 PM +00:00

--- pset7/movies/8.sql
+++ pset7/movies/8.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people WHERE id = (SELECT person_id FROM stars JOIN movies WHERE movie_id = id AND title LIKE "Toy Story%");
+SELECT name FROM people WHERE id = (SELECT person_id FROM stars JOIN movies ON movie_id = id AND title LIKE "Toy Story%");
