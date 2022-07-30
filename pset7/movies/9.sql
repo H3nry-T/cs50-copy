a502226e4a9bc4b02537d3780833d86e61a0d032
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-07-30 19:50:05 +0000

    Sat, Jul 30, 2022, 7:50 PM +00:00

--- pset7/movies/9.sql
+++ pset7/movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2014 ORDER BY ;
+SELECT name FROM people JOIN stars ON stars.person_id = people.id JOIN movies ON stars.movie_id = movies.id WHERE movies.year = 2014 ORDER BY people.birth;
