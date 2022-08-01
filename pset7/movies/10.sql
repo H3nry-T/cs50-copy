ac46544bd049b6c839cda577539f0d7ead2c6097
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 19:26:09 +0000

    Mon, Aug 1, 2022, 7:26 PM +00:00

--- pset7/movies/10.sql
+++ pset7/movies/10.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT name FROM people
+SELECT DISTINCT people.name FROM people
 JOIN directors ON people.id = directors.person_id
 JOIN movies ON directors.movie_id = movies.id
 JOIN ratings ON movies.id =ratings.movie_id
-ON ratings.rating = 9.0;
+WHERE ratings.rating >= 9.0;
