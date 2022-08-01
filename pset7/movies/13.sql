02c879ad004ea23a66c1ebe0c1e8bca662892b23
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:50:12 +0000

    Mon, Aug 1, 2022, 9:50 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -5,7 +5,7 @@ JOIN people ON stars.person_id = people.id
 WHERE name = "Kevin Bacon" AND birth = 1958;
 
 
-SELECT DISTINCT name FROM stars
+SELECT name FROM stars
 JOIN people ON stars.person_id = people.id
 WHERE movie_id IN
 (
@@ -13,7 +13,7 @@ WHERE movie_id IN
     JOIN stars ON movies.id = stars.movie_id
     JOIN people ON stars.person_id = people.id
     WHERE name = "Kevin Bacon" AND birth = 1958
-);
+)
 
-AND name != "Kevin Bacon"
+AND name != "Kevin Bacon";
 
