d9b8bb27a88db16ae5fcfdddb4dc0ae4803d99f8
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:28:49 +0000

    Mon, Aug 1, 2022, 9:28 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,9 +1,5 @@
-SELECT name FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.title = (
-    SELECT title FROM movies
-    JOIN stars ON movies.id = stars.movie_id
-    JOIN people ON stars.person_id = people.id
-    WHERE name = "Kevin Bacon"
-);
+
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE name = "Kevin Bacon"
