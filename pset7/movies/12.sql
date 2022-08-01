c706b4ef015c0aba6178760997ddb639c0040d1e
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:14:14 +0000

    Mon, Aug 1, 2022, 9:14 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -5,7 +5,7 @@ WHERE name = "Johhny Depp"
 
 INTERSECT
 
-SELECT title FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE name LIKE "Helena Bonham Carter"
+SELECT title FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE name = "Helena Bonham Carter"
