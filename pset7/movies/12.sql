05b68503187a72ddfbd9dd844eb0ff43ecc2a01f
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 20:15:56 +0000

    Mon, Aug 1, 2022, 8:15 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -3,4 +3,9 @@ WHERE id IN (
     SELECT movie_id FROM stars
     JOIN people ON stars.person_id = people.id
     WHERE people.name LIKE "Johnny Depp"
+    AND movie_id IN (
+        SELECT movie_id FROM stars
+        WHERE
+    )
 )
+ORDER BY title;
