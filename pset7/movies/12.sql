6d56053332d2144280945788da1d9fc0418f315a
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:20:16 +0000

    Mon, Aug 1, 2022, 9:20 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,11 +1,11 @@
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE name LIKE "Johnny Depp" 
+WHERE name LIKE "Johnny Depp"
 
 INTERSECT
 
-SELECT title FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
+SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
 WHERE name = "Helena Bonham Carter";
