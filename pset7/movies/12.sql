d5bec6b6c1d0490e0860629dd1fb56b8cbcbf8a2
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 20:17:20 +0000

    Mon, Aug 1, 2022, 8:17 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,11 +1,2 @@
-SELECT DISTINCT title FROM movies
-WHERE id IN (
-    SELECT movie_id FROM stars
-    JOIN people ON stars.person_id = people.id
-    WHERE people.name LIKE "Johnny Depp"
-    AND movie_id IN (
-        SELECT movie_id FROM stars
-        WHERE
-    )
-)
-ORDER BY title;
+SELECT title FROM movies
+
