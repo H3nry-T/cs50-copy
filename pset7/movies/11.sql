225db89ea279e952a9170ce0e7dfaa2d2fd64d61
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 19:51:15 +0000

    Mon, Aug 1, 2022, 7:51 PM +00:00

--- pset7/movies/11.sql
+++ pset7/movies/11.sql
@@ -3,6 +3,7 @@ JOIN ratings ON movies.id = ratings.movie_id
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE people.name LIKE "Chadwick Boseman"
-ORDER BY ratings.rating DESC;
+ORDER BY ratings.rating DESC
+LIMIT 5;
 
 
