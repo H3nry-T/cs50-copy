d7d07b0e72f2e973ac1b8b528ebd33ad131e0246
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:32:53 +0000

    Mon, Aug 1, 2022, 9:32 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -1,7 +1,7 @@
 
-SELECT title FROM movies
+SELECT movies.title, movies.id FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
 WHERE name = "Kevin Bacon" AND birth = 1958;
 
-SELECT 
+SELECT
