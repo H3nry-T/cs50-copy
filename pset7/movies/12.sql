691779863dca77831b10fcc79439e6a48855ade0
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:11:16 +0000

    Mon, Aug 1, 2022, 9:11 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,9 +1,8 @@
-SELECT title FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
+SELECT title FRP<
 WHERE name LIKE "Johhny Depp"
 
 INTERSECT
+
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
