b6a98b4dcebf91df6628e6399659bd35ca598a5b
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:13:04 +0000

    Mon, Aug 1, 2022, 9:13 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,7 +1,7 @@
 SELECT title FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-WHERE name LIKE "Johhny Depp"
+WHERE name = "Johhny Depp"
 
 INTERSECT
 
