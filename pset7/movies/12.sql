e522151977bf2c32c258b735f188eb3d9cca31a6
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 19:59:43 +0000

    Mon, Aug 1, 2022, 7:59 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,1 +1,5 @@
 SELECT title FROM movies
+JOIN stars ON movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id
+WHERE people.name LIKE "johnny depp"
+AND people.name LIKE "helena bonham carter"
