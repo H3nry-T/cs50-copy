5d2fc45e0a52c1282a371620763722f63f3d6cdf
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:15:28 +0000

    Mon, Aug 1, 2022, 9:15 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -8,4 +8,4 @@ INTERSECT
 SELECT title FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
-WHERE name = "Helena Bonham Carter"
+WHERE name = "Helena Bonham Carter";
