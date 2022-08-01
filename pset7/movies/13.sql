3215f7e4e339c96b7004ce294e6c01aef383f8a8
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 21:30:07 +0000

    Mon, Aug 1, 2022, 9:30 PM +00:00

--- pset7/movies/13.sql
+++ pset7/movies/13.sql
@@ -2,4 +2,4 @@
 SELECT title FROM movies
 JOIN stars ON movies.id = stars.movie_id
 JOIN people ON stars.person_id = people.id
-WHERE name = "Kevin Bacon"
+WHERE name = "Kevin Bacon" AND birth = 1958;
