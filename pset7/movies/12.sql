802284b9e1f47002db4fc9166af2a46d075ffab0
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-01 20:12:50 +0000

    Mon, Aug 1, 2022, 8:12 PM +00:00

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT title FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE people.name IN ("Johhny Depp", "Helena Bonham Carter")
-GROUP BY movies.title
-HAVING COUNT(movies.title) = 2;
+WHERE id IN (
+    SELECT movie_id FROM stars
+    JOIN people ON stars.person_id = people.id
+    WHERE people.name LIKE "Johnny Depp"
+)
