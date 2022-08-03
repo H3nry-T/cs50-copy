5b3bf0e26f22464de338f23965c3eab65e3c2992
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-03 17:02:49 +0000

    Wed, Aug 3, 2022, 5:02 PM +00:00

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -12,6 +12,8 @@ WHERE year = 2021 AND month = 7 AND day = 28;
 -- three witnesses
 
 .schema bakery_security_logs
+
+--take a look at the interview logs
 SELECT * FROM bakery_security_logs
 JOIN interviews ON bakery_security_logs.id = interviews.id
 WHERE interviews.year = 2021 AND interviews.month = 7 AND interviews.day = 28;
