dc33d9201dc118c4d9ed992d96c356cb90e2b03a
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-30 17:54:41 +0000

    Tue, Aug 30, 2022, 5:54 PM +00:00

---
+++ webtest/hamburger/hamburger.js
@@ -0,0 +1,13 @@
+const menuButton = document.querySelector(".menuButton");
+let menuOpen = false;
+menuButton.addEventListener("click", () => {
+    if(!menuOpen) {
+        menuButton.classList.add("open");
+        menuOpen = true;
+
+    }
+    else {
+        menuButton.classList.remove("open");
+        menuOpen = false;
+    }
+});
