25d04fdb6f50bf96e122dfe30b6fc52798d5e18d
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-30 18:26:54 +0000

    Tue, Aug 30, 2022, 6:26 PM +00:00

--- webtest/devman/menu.js
+++ webtest/devman/menu.js
@@ -2,13 +2,13 @@ cont hamburger = document.querySelector(".hamburger");
 cont navList = document.querySelector(".navList");
 
 hamburger.addEventListener("click", ()=> {
-    hamburger.classList.toggle(".active");
-    navList.classList.toggle(".active");
+    hamburger.classList.toggle("active");
+    navList.classList.toggle("active");
 });
 
 document.querySelectorAll(".navLink").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove(".active");
-    navList.classList.remove(".active");
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
 
 }));
 
