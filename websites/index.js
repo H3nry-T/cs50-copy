14fd04bc4a503e922323de4b9beae686c4fed8f2
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-08-31 17:25:27 +0000

    Wed, Aug 31, 2022, 5:25 PM +00:00

--- websites/index.js
+++ websites/index.js
@@ -3,5 +3,3 @@ const navToggle = document.querySelector('.navToggle');
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('navOpen')
 });
-
-document.querySelectorAll('.)
