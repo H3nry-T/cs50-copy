6e34c134784cd6f7a5d87852feef47fd4edd7ca6
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-09-21 12:23:25 +0000

    Wed, Sep 21, 2022, 12:23 PM +00:00

--- test/first_class_functions.py
+++ test/first_class_functions.py
@@ -5,5 +5,19 @@ def square(x):
     return x * x
 
 f = square(5)
+
 print(square)
 print(f)
+
+# now lets see what happens if f = square
+
+f = square
+
+print(square)
+print(f)
+
+# f is now a function.
+
+print(f(5))
+
+# we can now treat f just like any other function. 
