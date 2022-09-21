e19e5ba5113d59686bd523217f02769c4dd7f1b1
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-09-21 15:21:44 +0000

    Wed, Sep 21, 2022, 3:21 PM +00:00

--- test/closures.py
+++ test/closures.py
@@ -1,12 +1,16 @@
-def outer_function():
-    message = "Hi!"
+def outer_function(msg):
+    message = msg
 
     def inner_function():
         print(message)
     return inner_function
 
-my_func = outer_function()
+hi_func = outer_function("hi")
+
+hello_func = outer_function("hello")
 
 print(my_func.__name__)
 my_func()
-# closure is where an inner function can remember 
+# closure is where an inner function can remember variables within scope despite the outer function being excecuted.
+
+
