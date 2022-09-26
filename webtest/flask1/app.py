892134771f346b3a48dc3b084e29b912534ac2ca
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-09-26 19:47:30 +0000

    Mon, Sep 26, 2022, 7:47 PM +00:00

--- webtest/flask1/app.py
+++ webtest/flask1/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def index():
+    name = request.args.get("name")
     return render_template("index.html")
 
 
