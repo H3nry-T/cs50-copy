0c5e60122d00a987862f1819b75217fe198c406d
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-09-27 13:18:08 +0000

    Tue, Sep 27, 2022, 1:18 PM +00:00

--- webtest/flask1/app.py
+++ webtest/flask1/app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 
 def index():
     name = request.args.get("name")
     return render_template("index.html")
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
     name = request.args.get("name", "world")
     return render_template("greet.html", name_val=name)
