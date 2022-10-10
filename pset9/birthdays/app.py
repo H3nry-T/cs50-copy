4a1707d5d4a36d33ae6574fd18404810fbf2e798
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-10-10 19:21:01 +0000

    Mon, Oct 10, 2022, 7:21 PM +00:00

--- pset9/birthdays/app.py
+++ pset9/birthdays/app.py
@@ -27,7 +27,7 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-    
+
 
         return redirect("/")
 
@@ -36,6 +36,6 @@ def index():
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
 
-        return render_template("index.html")
+        return render_template("index.html", birthdays=birthdays)
 
 
