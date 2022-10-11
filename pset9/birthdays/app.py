f8bd7df45f86792dfa70e6b8055d29ed4e3b4678
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-10-11 13:28:11 +0000

    Tue, Oct 11, 2022, 1:28 PM +00:00

--- pset9/birthdays/app.py
+++ pset9/birthdays/app.py
@@ -31,11 +31,11 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
-        db.execute("INSERT INTO birthdays("name", "month", "day")
+        db.execute("INSERT INTO birthdays(name, month, day)")
 
 
         return redirect("/", name=name, month=month, day=day)
-        
+
 
     else:
 
