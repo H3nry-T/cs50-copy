96f3b54b8b70019655ff6bed5d509b83d13346fd
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-10-26 14:34:50 +0000

    Wed, Oct 26, 2022, 2:34 PM +00:00

--- webtest/login/app.py
+++ webtest/login/app.py
@@ -9,6 +9,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#handle redirects to the homepage if not name not provided
 @app.route("/")
 def index():
     if not session.get("name"):
