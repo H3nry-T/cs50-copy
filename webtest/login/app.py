717f54f2b0205f279bb932818ce7bff41cf5575b
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-10-26 14:32:39 +0000

    Wed, Oct 26, 2022, 2:32 PM +00:00

--- webtest/login/app.py
+++ webtest/login/app.py
@@ -7,5 +7,16 @@ app = Flask(__name__)
 #configure the session non permanent
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    if request.get("name"):
+        return redirect("/login")
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    
 
 print(__name__)
