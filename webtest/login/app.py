8d50110ac7326bff9087b6c77eabfa8d588d0ee0
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-10-26 14:29:14 +0000

    Wed, Oct 26, 2022, 2:29 PM +00:00

--- webtest/login/app.py
+++ webtest/login/app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template, redirect
+from flask_session import Session
 
+#configure the flask app
 app = Flask(__name__)
 
-print(__name__) 
+#configure the session non permanent
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+print(__name__)
