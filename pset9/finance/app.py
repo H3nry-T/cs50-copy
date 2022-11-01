d0d3d9bd32313b250283d46e38620a0c1e0a77aa
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-11-01 17:39:07 +0000

    Tue, Nov 1, 2022, 5:39 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -76,8 +76,9 @@ def buy():
             ); ''')
 
             value_of_stock = stock_price * stock_shares
+            current_cash = db.execute("SELECT * FROM users WHERE username = ?", username)
             db.execute("INSERT INTO portfolio (name, symbol, shares, price, value_of_stock) VALUES (?, ?, ?, ?, ?)", stock_name, stock_symbol, stock_shares, stock_price, value_of_stock)
-            return redirect("/") 
+            return redirect("/")
         except:
             return apology("invalid symbol")
 
