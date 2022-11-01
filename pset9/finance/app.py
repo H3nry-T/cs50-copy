56f30edb187170d3a0763ac2cbce3b97365cdc6f
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-11-01 21:51:59 +0000

    Tue, Nov 1, 2022, 9:51 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -110,10 +110,12 @@ def buy():
         # insert info into transaction history
         db.execute("INSERT INTO history (history_user_id, name, symbol, shares, price, value_of_stock) VALUES (?, ?, ?, ?, ?, ?)", session["user_id"], stock_name, stock_symbol, stock_shares, stock_price, value_of_stock)
 
+        # insert new stocks into portfolio
+        db.execute("INSERT INTO portfolio ())
         #take value_of_stock from transaction history; adding it up cumulatively
         new_value_row = db.execute("SELECT value_of_stock FROM history WHERE history_user_id = ? AND name = ?", session["user_id"], stock_name)
         old_value_row = db.execute("SELECT total_value_of_stock FROM portfolio WHERE portfolio_user_id = ? AND symbol = ?", session["user_id"], stock_symbol)
-        total_value_of_stock = new_value_row[0]["]
+        total_value_of_stock = 0
 
         for stock in new_value_row:
             total_value_of_stock += stock["value_of_stock"]
