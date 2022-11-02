dbbe22d154ea5c85a28db36c408fc8717e36bca9
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-11-02 14:36:17 +0000

    Wed, Nov 2, 2022, 2:36 PM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -309,6 +309,6 @@ def sell():
         updated_cash_balance = cash_balance[0]["cash"] + value_of_stock_sold
         db.execute("UPDATE users SET cash = ? WHERE id = ?", updated_cash_balance, session["user_id"])
 
-        # check portfolio DELETE stocks WHERE 
+        # check portfolio DELETE stocks WHERE id = session[id] AND shares = 0 
 
         return redirect("/")
