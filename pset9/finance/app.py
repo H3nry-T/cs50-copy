35433397252bb84f0b42de3af38b08197232a629
H3nry-T <108085805+H3nry-T@users.noreply.github.com>
2022-11-02 08:20:58 +0000

    Wed, Nov 2, 2022, 8:20 AM +00:00

--- pset9/finance/app.py
+++ pset9/finance/app.py
@@ -252,6 +252,9 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
+    
     if request.method == "GET":
         return render_template("sell.html")
+
+
     return apology("/sell in progress")
